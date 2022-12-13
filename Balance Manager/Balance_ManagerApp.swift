b674fecf6b1b57adb479fbbfec203a3e8ebc64e7
RJ <radek.jenik36@gmail.com>
2022-12-13 12:58:14 +0100

    Initial Commit

---
+++ Balance Manager/Balance_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Balance_ManagerApp.swift
+//  Balance Manager
+//
+//  Created by Radek Jeník on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Balance_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
