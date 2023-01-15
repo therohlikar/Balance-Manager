4e803c05a189f67bc9780ab2d7af25b6bb754617
RJ <radek.jenik36@gmail.com>
2023-01-15 09:57:52 +0100

    swipe left to right to go back

--- Balance Manager/Views/SelectProfileView.swift
+++ Balance Manager/Views/SelectProfileView.swift
@@ -85,3 +85,14 @@ struct SelectProfileView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
